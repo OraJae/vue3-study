91403631708bf5603150ab42c6cd10495faf78c2
zhangjie <464060360@qq.com>
2024-05-06 09:05:53 +0800

    路由调整

--- src/router/constant.ts
+++ src/router/constant.ts
@@ -2,80 +2,89 @@ export default [
     {
         path: '/',
         name: 'index',
-        component: () => import('@/views/index.vue'),
-        // redirect: '/',
-    },
-    {
-        path: '/seal',
-        name: 'seal',
-        meta: {
-            title: 'pdf印章'
-        },
-        component: () => import('@/views/seal/index.vue'),
-    },
-    {
-        path: '/sheet',
-        name: 'sheet',
-        meta: {
-            title: 'sheet表格'
-        },
-        component: () => import('@/views/sheet/index.vue'),
-    },
-    {
-        path: '/h5/pageCache/list',
-        name: 'h5PageCacheList',
-        meta: {
-            title: 'h5页面缓存方案'
-        },
-        component: () => import('@/views/h5/pageCache/index.vue'),
-    },
-    {
-        path: '/h5/pageCache/detail',
-        name: 'h5PageCacheDetail',
-        meta: {
-            title: 'h5页面缓存方案详情'
-        },
-        component: () => import('@/views/h5/pageCache/detail.vue'),
-    },
-    {
-        path: '/h5/pageLayout',
-        name: 'h5PageLayout',
-        meta: {
-            title: 'h5页面布局组件'
-        },
-        component: () => import('@/views/h5/pageLayout/index.vue'),
-    },
-    {
-        path: '/h5/iframe/postMessage',
-        name: 'h5IframePostMessage',
-        meta: {
-            title: 'iframe father'
-        },
-        component: () => import('@/views/h5/iframe/postMessage/index.vue'),
-    },
-    {
-        path: '/h5/iframe/postMessage/son',
-        name: 'h5IframePostMessageSon',
-        meta: {
-            title: 'iframe son'
-        },
-        component: () => import('@/views/h5/iframe/postMessage/son.vue'),
-    },
-    {
-        path: '/h5/iframe/postMessage/grandson',
-        name: 'h5IframePostMessageGrandson',
-        meta: {
-            title: 'iframe grandson'
-        },
-        component: () => import('@/views/h5/iframe/postMessage/grandson.vue'),
-    },
-    {
-        path: '/h5/vant',
-        name: 'h5Vant',
-        meta: {
-            title: 'h5Vant组件'
-        },
-        component: () => import('@/views/h5/vant/index.vue'),
+        redirect: '/home',
+        children: [
+            {
+                path: 'home',
+                name: 'home',
+                meta: {
+                    title: '首页'
+                },
+                component: () => import('@/views/index.vue'),
+            },
+            {
+                path: 'seal',
+                name: 'seal',
+                meta: {
+                    title: 'pdf印章'
+                },
+                component: () => import('@/views/seal/index.vue'),
+            },
+            {
+                path: 'sheet',
+                name: 'sheet',
+                meta: {
+                    title: 'sheet表格'
+                },
+                component: () => import('@/views/sheet/index.vue'),
+            },
+            {
+                path: 'h5/pageCache/list',
+                name: 'h5PageCacheList',
+                meta: {
+                    title: 'h5页面缓存方案'
+                },
+                component: () => import('@/views/h5/pageCache/index.vue'),
+            },
+            {
+                path: 'h5/pageCache/detail',
+                name: 'h5PageCacheDetail',
+                meta: {
+                    title: 'h5页面缓存方案详情'
+                },
+                component: () => import('@/views/h5/pageCache/detail.vue'),
+            },
+            {
+                path: 'h5/pageLayout',
+                name: 'h5PageLayout',
+                meta: {
+                    title: 'h5页面布局组件'
+                },
+                component: () => import('@/views/h5/pageLayout/index.vue'),
+            },
+            {
+                path: 'h5/iframe/postMessage',
+                name: 'h5IframePostMessage',
+                meta: {
+                    title: 'iframe father'
+                },
+                component: () => import('@/views/h5/iframe/postMessage/index.vue'),
+            },
+            {
+                path: 'h5/iframe/postMessage/son',
+                name: 'h5IframePostMessageSon',
+                meta: {
+                    title: 'iframe son'
+                },
+                component: () => import('@/views/h5/iframe/postMessage/son.vue'),
+            },
+            {
+                path: 'h5/iframe/postMessage/grandson',
+                name: 'h5IframePostMessageGrandson',
+                meta: {
+                    title: 'iframe grandson'
+                },
+                component: () => import('@/views/h5/iframe/postMessage/grandson.vue'),
+            },
+            {
+                path: 'h5/vant',
+                name: 'h5Vant',
+                meta: {
+                    title: 'h5Vant组件'
+                },
+                component: () => import('@/views/h5/vant/index.vue'),
+            },
+        ]
     },
     // {
     //     path: '/h5',
