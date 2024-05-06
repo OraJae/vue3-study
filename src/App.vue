86ec38374842b2e145ac9cc0d322c6bd44582b81
zhangjie <464060360@qq.com>
2024-05-06 09:24:48 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   123
-  <router-view v-slot="{ Component }" :max="10">
+  <!-- <router-view v-slot="{ Component }" :max="10">
     <keep-alive :include="caches">
       <component :is="Component" />
     </keep-alive>
-  </router-view>
-  <!-- <router-view></router-view> -->
+  </router-view> -->
+  <router-view></router-view>
 </template>
 
 <script setup lang="ts">
