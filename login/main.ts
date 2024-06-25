ce6430b0768dbebce59ebb321a49b7499d6f2ad4
15629006909 <464060360@qq.com>
2024-06-25 17:08:05 +0800

    增加登陆页

---
+++ login/main.ts
@@ -0,0 +1,9 @@
+import { createApp } from "vue"
+import App from "./app.vue"
+
+import Antd from "ant-design-vue"
+
+const app = createApp(App)
+app.use(Antd)
+
+app.mount("#app")
--- src/main.js
+++ src/main.js
@@ -1,15 +1,23 @@
-import { createApp } from "vue"
-import App from "./App.vue"
-import router from "./router/index"
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router/index'
 
-import Antd from "ant-design-vue"
-import Vant from "vant"
-import "vant/lib/index.css"
+import Antd from 'ant-design-vue'
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
 // Vue.use(List);
-import "@styles/index.less"
+import '@styles/index.less'
 // 内置组件
-import LayoutComponent from "./layout"
+import LayoutComponent from './layout'
+
+router.beforeEach(async (to, from, next) => {
+  if (decodeURIComponent(sessionStorage.getItem('token')) == 'admin321') {
+    next()
+  } else {
+    location.href = '/login.html'
+  }
+})
 
 const app = createApp(App)
 // app.use(ElementPlus)
@@ -18,4 +26,4 @@ app.use(Antd)
 app.use(Vant)
 app.use(LayoutComponent)
 
-app.mount("#app")
+app.mount('#app')
